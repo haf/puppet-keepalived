55bf76a772a0ccb745a22f475cfef3567283e0bc
Ronny Roethof <ronny@roethof.net>
2012-05-24 19:49:42 +0200

    made module more independent

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,12 +1,13 @@
-class keepalived::base {
+define keepalived::base($emailto, $emailfrom, $smtpserver, $interface, $state, $priority, $auth_pass, $virtual_ips, $vrrp_script) {
 
-  require => Package['keepalived'];
-
-  file{'/etc/keepalived/keepalived.conf':
-    owner => root,
+  file{ '/etc/keepalived/keepalived.conf':
+    owner => root, 
     group => root,
     mode => 600,
+    content => template("keepalived/keepalived.conf.erb"),
     notify => Service['keepalived'],
+    require => Package['keepalived']
   }
 
 } 
+
